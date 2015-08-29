7d25d29b0471d132f632f12a5f800a370243eba1
Squareys <Squareys@googlemail.com>
2015-08-30 01:12:25 +0200

    Audio: Expand Source test and fix bugs revealed in the process
    Only minor copy past bugs introduced when implementing the getters.
    
    Signed-off-by: Squareys <Squareys@googlemail.com>

--- src/Magnum/Audio/Test/SourceTest.cpp
+++ src/Magnum/Audio/Test/SourceTest.cpp
@@ -26,6 +26,7 @@
 #include <sstream>
 #include <Corrade/TestSuite/Tester.h>
 
+#include "Magnum/Audio/Context.h"
 #include "Magnum/Audio/Source.h"
 
 namespace Magnum { namespace Audio { namespace Test {
@@ -34,10 +35,34 @@ struct SourceTest: TestSuite::Tester {
     explicit SourceTest();
 
     void debugState();
+    void position();
+    void direction();
+    void velocity();
+    void gain();
+    void looping();
+    void relative();
+    void maxDistance();
+    void maxGain();
+    void minGain();
+    void coneAnglesAndGain();
+    void rolloffFactor();
+
+    Context _context;
 };
 
-SourceTest::SourceTest() {
-    addTests({&SourceTest::debugState});
+SourceTest::SourceTest(): _context{} {
+    addTests({&SourceTest::debugState,
+              &SourceTest::position,
+              &SourceTest::direction,
+              &SourceTest::velocity,
+              &SourceTest::gain,
+              &SourceTest::looping,
+              &SourceTest::relative,
+              &SourceTest::maxDistance,
+              &SourceTest::maxGain,
+              &SourceTest::minGain,
+              &SourceTest::coneAnglesAndGain,
+              &SourceTest::rolloffFactor});
 }
 
 void SourceTest::debugState() {
@@ -45,6 +70,102 @@ void SourceTest::debugState() {
     Debug(&out) << Source::State::Playing;
     CORRADE_COMPARE(out.str(), "Audio::Source::State::Playing\n");
 }
+
+void SourceTest::position() {
+    Source source{};
+    constexpr Vector3 pos{3, 5, 6};
+    source.setPosition(pos);
+
+    CORRADE_COMPARE(source.position(), pos);
+}
+
+void SourceTest::direction() {
+    Source source{};
+    constexpr Vector3 dir{3, 1, 2};
+    source.setDirection(dir);
+
+    CORRADE_COMPARE(source.direction(), dir);
+}
+
+void SourceTest::velocity() {
+    Source source{};
+    constexpr Vector3 vel{-3, 5, -6};
+    source.setVelocity(vel);
+
+    CORRADE_COMPARE(source.velocity(), vel);
+}
+
+void SourceTest::gain() {
+    Source source{};
+    constexpr Float gain = 0.1234;
+    source.setGain(gain);
+
+    CORRADE_COMPARE(source.gain(), gain);
+}
+
+void SourceTest::looping() {
+    Source source{};
+    source.setLooping(true);
+    CORRADE_COMPARE(true, source.isLooping());
+    source.setLooping(false);
+    CORRADE_COMPARE(false, source.isLooping());
+}
+
+void SourceTest::relative() {
+    Source source{};
+    source.setRelative(true);
+    CORRADE_COMPARE(true, source.isRelative());
+    source.setRelative(false);
+    CORRADE_COMPARE(false, source.isRelative());
+}
+
+void SourceTest::maxDistance() {
+    Source source{};
+    constexpr Float dist = 0.222;
+    source.setMaxDistance(dist);
+
+    CORRADE_COMPARE(source.maxDistance(), dist);
+}
+
+void SourceTest::maxGain() {
+    Source source{};
+    constexpr Float gain = 0.3131;
+    source.setMaxGain(gain);
+
+    CORRADE_COMPARE(source.maxGain(), gain);
+}
+
+void SourceTest::minGain() {
+    Source source{};
+    constexpr Float gain = 0.4144;
+    source.setMinGain(gain);
+
+    CORRADE_COMPARE(source.minGain(), gain);
+}
+
+void SourceTest::coneAnglesAndGain() {
+    Source source{};
+    constexpr Deg outerAngle = Deg(12);
+    constexpr Deg innerAngle = Deg(21);
+    constexpr Float outerGain = 0.05;
+
+    source.setInnerConeAngle(innerAngle);
+    source.setOuterConeAngle(outerAngle);
+    source.setOuterConeGain(outerGain);
+
+    CORRADE_COMPARE(source.outerConeAngle(), outerAngle);
+    CORRADE_COMPARE(source.innerConeAngle(), innerAngle);
+    CORRADE_COMPARE(source.outerConeGain(), outerGain);
+}
+
+void SourceTest::rolloffFactor() {
+    Source source{};
+    constexpr Float fact = 42.0f;
+    source.setRolloffFactor(fact);
+
+    CORRADE_COMPARE(source.rolloffFactor(), fact);
+}
+
 
 }}}
 
